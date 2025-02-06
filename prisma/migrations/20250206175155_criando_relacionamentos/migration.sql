425d4615667d2ecddba75e62c2164c96da5ae7d5
eutiagosantos <tiagoalmeidasantos3412@gmail.com>
2025-02-06 14:52:23 -0300

    criando relacionamento entre as tabelas

---
+++ prisma/migrations/20250206175155_criando_relacionamentos/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - Added the required column `password_hash` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "password_hash" TEXT NOT NULL,
+ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "gyms" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "phone" TEXT,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "gyms_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "check_ins" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "validated_at" TIMESTAMP(3),
+    "userId" TEXT NOT NULL,
+    "gymId" TEXT NOT NULL,
+
+    CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_gymId_fkey" FOREIGN KEY ("gymId") REFERENCES "gyms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
